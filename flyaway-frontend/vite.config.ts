96a614d336572b17617ca720f6ca0f56a6693fd7
GiovannaLani <giovanna.lani@opendeusto.es>
2026-01-10 20:22:13 +0100

    vite config

--- flyaway-frontend/vite.config.ts
+++ flyaway-frontend/vite.config.ts
@@ -9,5 +9,9 @@ console.log("VITE ENV:", process.env.VITE_API_URL)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+    'import.meta.env.VITE_ASSETS_URL': JSON.stringify(process.env.VITE_ASSETS_URL),
+  }
 })
 
